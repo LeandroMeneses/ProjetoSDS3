b8e18c4065434a28fefcaf036a8a2b7f4f168457
Leandro Meneses <souza.meneses@gmail.com>
2021-11-07 13:31:40 -0300

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => { // a função pode ser declarada assim também
                 <div className="container">
                     <p className="text-light">App desenvolvido por <a href="https://github.com/LeandroMeneses" target="_blank" rel="noreferrer">Leandro Meneses</a></p>
                     <p className="text-light"><small><strong>Semana Spring React - Semana top de estudos</strong><br />
-                        Me contacte: <a href="https://www.linkedin.com/in/devleandromeneses/" target="_blank" rel="noreferrer">@LeandroMeneses</a></small></p>
+                        Me contrate: <a href="https://www.linkedin.com/in/devleandromeneses/" target="_blank" rel="noreferrer">@LeandroMeneses</a></small></p>
                 </div>
             </footer>
         </div>
